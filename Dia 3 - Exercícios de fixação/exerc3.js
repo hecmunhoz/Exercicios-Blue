82d6be24d3a1e0d8b9eb80c408f613b84443a1c1
Hector Munhoz <81196539+hecmunhoz@users.noreply.github.com>
2021-08-18 22:13:07 -0300

    Add files via upload

---
+++ Dia 3 - Exercícios de fixação/exerc3.js
@@ -0,0 +1,91 @@
+// 3. Letra de música - Faça um programa que mostre na tela uma letra de música que você gosta (proibido letras do Justin Bieber).
+
+console.log(`Justin Bieber - Baby\n
+Oh, woah
+Oh, woah
+Oh, woah
+
+You know you love me, I know you care
+Just shout whenever, and I'll be there
+You are my love, you are my heart
+And we will never, ever, ever be apart
+
+Are we an item? Girl, quit playing!
+We're just friends? What are you saying?
+Said there's another and looked right in my eyes
+My first love broke my heart for the first time
+
+And I was like
+Baby, baby, baby, ooh
+Like baby, baby, baby, no
+Like baby, baby, baby, ooh
+Thought you'd always be mine, mine
+
+Baby, baby, baby, ooh
+Like baby, baby, baby, no
+Like baby, baby, baby, ooh
+I thought you'd always be mine
+
+For you, I would have done whatever
+And I just can't believe, we ain't together
+And I wanna play it cool, but I'm losin' you
+I'll buy you anything, I'll buy you any ring
+And I'm in pieces, baby fix me
+And just shake me til' you wake me from this bad dream
+
+I'm going down, down, down
+And I just can't believe
+My first love won't be around
+
+And I'm like
+Baby, baby, baby, ooh
+Like baby, baby, baby, no
+Like baby, baby, baby, ooh
+I thought you'd always be mine
+
+Baby, baby, baby, ooh
+Like baby, baby, baby, no
+Like baby, baby, baby, ooh
+I thought you'd always be mine
+
+Luda!
+When I was thirteen
+I had my first love
+There was nobody that compared to my baby
+And nobody came to between us or could ever come above
+She had me going crazy
+
+Oh, I starstruck
+She woke me up daily
+Don't need no Starbucks
+She make my heart pound
+I skip a beat when
+I see her in the street and
+At school on the playground
+But I really wanna
+See her on the weekend
+She knows she got me dazing
+Cuz she was so amazing
+And now my heart is breaking
+But I just keep on saying
+
+Baby, baby, baby, ooh
+Like baby, baby, baby, no
+Like baby, baby, baby, ooh
+I thought you'd always be mine
+
+Baby, baby, baby, ooh
+Like baby, baby, baby, no
+Like baby, baby, baby, ooh
+I thought you'd always be mine, mine
+
+And I was like
+Baby, baby, baby, ooh
+Like baby, baby, baby, no
+Like baby, baby, baby, ooh
+I thought you'd always be mine, mine
+
+I'm gone, yeah, yeah, yeah
+Now I'm all gone, yeah, yeah, yeah
+Now I'm all gone, yeah, yeah, yeah
+Now I'm all gone, gone, gone, gone, I'm gone`);
