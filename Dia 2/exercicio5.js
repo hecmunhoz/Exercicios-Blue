a3a2671315d2e83b65ab056072dffce7625fece6
Hector Munhoz <81196539+hecmunhoz@users.noreply.github.com>
2021-08-18 21:08:41 -0300

    Update exercicio5.js

--- Dia 2/exercicio5.js
+++ Dia 2/exercicio5.js
@@ -4,4 +4,4 @@ let x = parseFloat(prompt("Digite a primeira nota: "));
 let y = parseFloat(prompt("Digite a segunda nota: "));
 let z = parseFloat(prompt("Digite a terceira nota: "));
 
-console.log("Com as notas", x, "+", y, "+", z, ", você obteve a média de:", ((x+y+z)/3).toFixed(2));
+console.log(`Com as notas ${x} + ${y} + ${z}, você obteve a média de: ${((x+y+z)/3).toFixed(2)}`);
