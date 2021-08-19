10e505918ac32907e319c45a0bf32beb78ce27b0
Hector Munhoz <81196539+hecmunhoz@users.noreply.github.com>
2021-08-18 21:09:52 -0300

    Update exercicio6.js

--- Dia 2/exercicio6.js
+++ Dia 2/exercicio6.js
@@ -2,4 +2,4 @@
 
 let x = parseInt(prompt("Enter the first integer: "));
 let y = parseInt(prompt("Enter the second integer: "));
-console.log("Is %i greater than or equal to %i?", x, y, x>=y);
+console.log(`Is ${x} greater than or equal to ${y}? ${x>=y}`);
