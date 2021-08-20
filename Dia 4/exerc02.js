339961e8e03f1e36656f2efc2c08600262356b2d
Hector Munhoz <81196539+hecmunhoz@users.noreply.github.com>
2021-08-19 21:32:18 -0300

    Update exerc02.js

--- Dia 4/exerc02.js
+++ Dia 4/exerc02.js
@@ -1,6 +1,8 @@
 /*DESAFIO 02
 Jogo da adivinhação
-Escreva um programa que faça o computador “pensar” em um número inteiro entre 0 e 10 e peça para o usuário tentar descobrir qual foi o número escolhido pelo computador. O programa deverá escrever na tela se o usuário venceu ou perdeu.*/
+Escreva um programa que faça o computador "pensar" em um número inteiro entre 0 e 10 e peça para o usuário tentar descobrir qual foi o número escolhido pelo computador. 
+O programa deverá escrever na tela se o usuário venceu ou perdeu.
+*/
 
 let numeroAleatorio = Math.floor(Math.random() * 11);
 // console.log(numeroAleatorio);
