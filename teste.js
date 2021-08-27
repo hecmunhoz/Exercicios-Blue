e66fd4d753086a07f571c647e59568f02ecf852a
Hector Munhoz <hectormunhozc@gmail.com>
2021-08-27 00:11:55 -0300

    Update teste.js

--- teste.js
+++ teste.js
@@ -1,1 +1,27 @@
 // oi
+
+// let carro = {modelo: "pee", ano: 2020};
+// for (i in carro){
+//     console.log(`${i} : ${carro[i]}`);
+// }
+
+
+let lista = ["A","B","C","D","E"]; 
+
+for (let i = 0; i < lista.length; i++){
+    console.log(`${i} : ${lista[i]}`)
+}
+for (let i in lista){
+    console.log(`${i} : ${lista[i]}`);
+}
+
+let g = 0;
+for (let i of lista){
+    console.log(`${g} : ${i}`);
+    g += 1
+}
+
+lista.forEach(printar);
+function printar(valor, indice){
+    console.log(`${indice} : ${valor}`);
+}
