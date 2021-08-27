e9a1d5b3ee3f01a492968a3f7fa2195695580328
Hector Munhoz <81196539+hecmunhoz@users.noreply.github.com>
2021-08-26 23:53:29 -0300

    Update projeto2.js

--- Projeto 2/projeto2.js
+++ Projeto 2/projeto2.js
@@ -7,20 +7,12 @@
 //     Decidir de forma aleatória a decisão do computador;
 //     Mostrar quantas rodadas cada jogador ganhou;
 //     Determinar quem foi o grande campeão de acordo com a quantidade de vitórias de cada um (computador e jogador);
-//     Perguntar se o Jogador quer jogar novamente, se sim inicie volte a escolha de quantidade de rodadas, se não finalize o programa.
-
-// Critérios de avaliação:
-//     Permitir que eu decida quantas rodadas iremos fazer (1 ponto) 
-//     Ler a minha escolha: `pedra`, `papel` ou `tesoura` (2 pontos) 
-//     Decidir de forma aleatória a decisão do computador (2 pontos)
-//     Mostrar quantas rodadas cada jogador ganhou (2 pontos)
-//     Determinar quem foi o grande campeão de acordo com a quantidade de vitórias de cada um (computador e jogador) (1,5 pontos)
-//     Perguntar se o Jogador quer jogar novamente, se sim inicie volte a escolha de quantidade de rodadas, se não finalize o programa. (1,5 pontos)
-const prompt = require('prompt-sync')();
+//     Perguntar se o jogador quer jogar novamente, se sim volte à escolha de quantidade de rodadas, se não finalize o programa.
 
 console.log("\n------ JOKENPÔ ------");
 console.log("    PEDRA   = 0\n    PAPEL   = 1\n    TESOURA = 2\n");
 
+const prompt = require('prompt-sync')();
 const jogador = prompt("Por favor, digite seu nome: ").toUpperCase();
 let repetir = true;
 let novamente = " ";
@@ -109,37 +101,3 @@ function quemGanhou(j1,j2){
         }
     }
 }
-
-/*
-if rodadas % 2 == 0 {
-rodadas = 10
-
-vitorias = 2
-
-// vc venceu 6   e o pc venceu 4
-
-// 4+6 = 10
-
-`Você venceu ${Math.ceil(rodadas/2+vitorias/2)} e o computador venceu ${Math.floor(rodadas/2-vitorias/2)}. `
-} else {
-rodadas = 9
-vitorias = 3
-// vc venceu 6 e o pc venceu 3
-// 4+5=9
-
-rodadas = 3
-
-}
-*/
-
-/*
-
-pedra = 0
-papel = 1
-tesoura = 2
-
-pedra > tesoura > papel
-
-*/
-
-// If it is the same (two odd-numbered moves or two even-numbered ones) then the lower number wins, while if they are different (one odd and one even) the higher wins.
